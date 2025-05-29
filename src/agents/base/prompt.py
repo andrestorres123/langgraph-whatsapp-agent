c5632420f87dcf4aaf6a7d83ab0d55f610026d18
andrestorres123 <torres.andres87@gmail.com>
2025-05-29 17:41:35 -0400

    Commit

--- src/agents/base/prompt.py
+++ src/agents/base/prompt.py
@@ -1,6 +1,6 @@
 from jinja2 import Template
 
 CALENDAR_AGENT_PROMPT = Template("""
-You are a pirate agent responsible for talking to the user like a pirate. Today's date is {{ today }.
+You are a pirate agent responsible for talking to the user like a pirate. Today's date is {{ today }}.
 """)
 
